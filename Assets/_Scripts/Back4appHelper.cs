dd8e653684b86b00c99e4784a4409a28e1608bab
Stanislav <99rsw99@mail.ru>
2020-10-09 15:18:29 +0300

    Before Add Back4App Library

--- Assets/_Scripts/Back4appHelper.cs
+++ Assets/_Scripts/Back4appHelper.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using UnityEngine.UI;
 
 public class Back4appHelper : MonoBehaviour
@@ -15,32 +17,81 @@ public class Back4appHelper : MonoBehaviour
     public static string COLORS = "Colors";
     public static string GRAPES = "Grapes";
     public static string VINE = "Vine";
-    
+    public delegate void OnEndAddDataCallback(AddDataCallback data);
+    public delegate void OnEndGetDataCallback(GetDataCallback data);
+
     [SerializeField] private string parseApplicationId;
     [SerializeField] private string restApiKey;
-    [SerializeField] private Texture2D testImage;
+    [SerializeField] private UnityEngine.UI.Image testImage;
 
-    private string testImageUrl =
-        "https://parsefiles.back4app.com/3wnDQP64MYfQ9VRJub2SFyepUpPxw0bkK1YKIzeI/2f50e0f41d28ab65d2cee56677432013_TestPicture.jpg";
-
-    private string testImageName = "2f50e0f41d28ab65d2cee56677432013_TestPicture.jpg";
+    //private string testImageUrl =
+    //    "https://parsefiles.back4app.com/3wnDQP64MYfQ9VRJub2SFyepUpPxw0bkK1YKIzeI/2f50e0f41d28ab65d2cee56677432013_TestPicture.jpg";
+    //private string testImageName = "2f50e0f41d28ab65d2cee56677432013_TestPicture.jpg";
 
     private void Start()
     {
-        StartCoroutine(AddVine(testImageUrl, testImageName, color: "Test"));
+        //StartCoroutine(DownloadImageFromServerCor());
     }
 
-    IEnumerator AddColor(string color)
+    public void AddColor(string color, OnEndAddDataCallback onEndAddDataCallback)
+    {
+        StartCoroutine(AddColorCor(color, onEndAddDataCallback));
+    }
+
+    public void GetColors(OnEndGetDataCallback onEndGetDataCallback)
+    {
+        StartCoroutine(GetColorsCor(onEndGetDataCallback));
+    }
+
+    public void AddCountry(string country, OnEndAddDataCallback onEndAddDataCallback)
+    {
+        StartCoroutine(AddCountryCor(country, onEndAddDataCallback));
+    }
+
+    public void GetCountries(OnEndGetDataCallback onEndGetDataCallback)
+    {
+        StartCoroutine(GetCountriesCor(onEndGetDataCallback));
+    }
+
+    public void AddRegion(string region, OnEndAddDataCallback onEndAddDataCallback)
+    {
+        StartCoroutine(AddRegionCor(region, onEndAddDataCallback));
+    }
+
+    public void GetRegions(OnEndGetDataCallback onEndGetDataCallback)
+    {
+        StartCoroutine(GetRegionsCor(onEndGetDataCallback));
+    }
+
+    public void AddGrape(string grape, OnEndAddDataCallback onEndAddDataCallback)
+    {
+        StartCoroutine(AddGrapeCor(grape, onEndAddDataCallback));
+    }
+
+    public void GetGrapes(OnEndGetDataCallback onEndGetDataCallback)
+    {
+        StartCoroutine(GetGrapesCor(onEndGetDataCallback));
+    }
+
+    public void AddVine(Texture2D image, OnEndAddDataCallback onEndAddDataCallback, string color = "",  string grape = "", string country = "", string region = "")
+    {
+        StartCoroutine(UploadImageToServerCor(image, color, grape, country, region, onEndAddDataCallback));
+    }
+
+    IEnumerator AddColorCor(string color, OnEndAddDataCallback onEndAddDataCallback)
     {
         var data = CreateDataFromString("Color", color);
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + COLORS);
         www.method = "POST";
         www.uploadHandler = new UploadHandlerRaw(UTF8Encoding.UTF8.GetBytes(data));
+        www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
+        
+        onEndAddDataCallback(new AddDataCallback(www.downloadHandler.text, www.responseCode));
     }
 
-    IEnumerator GetColors()
+    IEnumerator GetColorsCor(OnEndGetDataCallback onEndGetDataCallback)
     {
         UnityWebRequest www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + COLORS);
         www.downloadHandler = new DownloadHandlerBuffer();
@@ -48,70 +99,81 @@ public class Back4appHelper : MonoBehaviour
         yield return www.SendWebRequest();
         
         print(www.downloadHandler.text);
+        
+        onEndGetDataCallback(new GetDataCallback(www.downloadHandler.text, www.responseCode));
 
     }
     
-    IEnumerator AddCountry(string country)
+    IEnumerator AddCountryCor(string country, OnEndAddDataCallback onEndAddDataCallback)
     {
         var data = CreateDataFromString("Country", country);
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + COUNTRIES);
         www.method = "POST";
         www.uploadHandler = new UploadHandlerRaw(UTF8Encoding.UTF8.GetBytes(data));
+        www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
+        
+        onEndAddDataCallback(new AddDataCallback(www.downloadHandler.text, www.responseCode));
     }
     
-    IEnumerator GetCountries(string country)
+    IEnumerator GetCountriesCor(OnEndGetDataCallback onEndGetDataCallback)
     {
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + COUNTRIES);
         www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
         
-        print(www.downloadHandler.text);
+        onEndGetDataCallback(new GetDataCallback(www.downloadHandler.text, www.responseCode));
     }
 
-    IEnumerator AddRegion(string region)
+    IEnumerator AddRegionCor(string region, OnEndAddDataCallback onEndAddDataCallback)
     {
         var data = CreateDataFromString("Region", region);
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + REGIONS);
         www.method = "POST";
         www.uploadHandler = new UploadHandlerRaw(UTF8Encoding.UTF8.GetBytes(data));
+        www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
+        
+        onEndAddDataCallback(new AddDataCallback(www.downloadHandler.text, www.responseCode));
     }
     
-    IEnumerator GetRegions(string country)
+    IEnumerator GetRegionsCor(OnEndGetDataCallback onEndGetDataCallback)
     {
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + REGIONS);
         www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
         
-        print(www.downloadHandler.text);
+        onEndGetDataCallback(new GetDataCallback(www.downloadHandler.text, www.responseCode));
     }
     
-    IEnumerator AddGrape(string grape)
+    IEnumerator AddGrapeCor(string grape, OnEndAddDataCallback onEndAddDataCallback)
     {
         var data = CreateDataFromString("Grape", grape);
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + GRAPES);
         www.method = "POST";
         www.uploadHandler = new UploadHandlerRaw(UTF8Encoding.UTF8.GetBytes(data));
+        www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
+        
+        onEndAddDataCallback(new AddDataCallback(www.downloadHandler.text, www.responseCode));
     }
     
-    IEnumerator GetGrapes(string grape)
+    IEnumerator GetGrapesCor(OnEndGetDataCallback onEndGetDataCallback)
     {
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + GRAPES);
         www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
         
-        print(www.downloadHandler.text);
+        onEndGetDataCallback(new GetDataCallback(www.downloadHandler.text, www.responseCode));
     }
     
-    IEnumerator AddVine(string imageUrl, string imageName, string color = "",  string grape = "", string country = "", string region = "")
+    IEnumerator AddVineCor(string imageUrl, string imageName, string color,  string grape, string country, string region, OnEndAddDataCallback onEndAddDataCallback)
     {
         Dictionary<string, string> data = new Dictionary<string, string>();
         if (color != "")
@@ -124,53 +186,68 @@ public class Back4appHelper : MonoBehaviour
             data.Add("Region", region);
 
         var imageData = string.Format("{{\"name\":\"{0}\",\"url\":\"{1}\",\"__type\":\"File\"}}", imageName, imageUrl);
-            /*CreateDataFromDict(new Dictionary<string, string>()
-        {
-            {"name", imageName},
-            {"url", imageUrl},
-            {"__type", "File"}
-        });*/
+        
         data.Add("Image", imageData);
         var www = CreateRequestAndSetHeaders("https://parseapi.back4app.com/classes/" + VINE);
         www.method = "POST";
         www.downloadHandler = new DownloadHandlerBuffer();
         www.uploadHandler = new UploadHandlerRaw(UTF8Encoding.UTF8.GetBytes(CreateDataFromDict(data)));
-        print(CreateDataFromDict(data));
+        //print(CreateDataFromDict(data));
 
         yield return www.SendWebRequest();
 
-        if (www.isHttpError)
-        {
-            print(www.error);
-        }
-        else
-        {
-            print(www.downloadHandler.text);
-        }
+        print(www.downloadHandler.text);
+        onEndAddDataCallback(new AddDataCallback(www.downloadHandler.text, www.responseCode));
     }
 
-    IEnumerator UploadImageToServer(string fileName, Texture2D image)
+    IEnumerator DownloadImageFromServerCor(string url)
+    {
+        var www = UnityWebRequestTexture.GetTexture(url);
+        //ww.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return www.SendWebRequest();
+
+        Texture2D tex = DownloadHandlerTexture.GetContent(www);
+        testImage.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+        System.IO.File.WriteAllBytes( Application.dataPath + "/UnityImage.jpg", tex.EncodeToJPG());
+        print(Application.dataPath);
+    }
+
+    IEnumerator UploadImageToServerCor(Texture2D image, string color, string grape, string country, string region, OnEndAddDataCallback onEndAddDataCallback)
     {
         List<IMultipartFormSection> data = new List<IMultipartFormSection>();
+        /*yield return new WaitForEndOfFrame();
+        int width = Screen.width;
+        int height = Screen.height;
+        Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();*/
         data.Add(new MultipartFormFileSection(image.EncodeToJPG()));
-        //WWWForm form = new WWWForm();
-        //form.AddBinaryData("Image", image.EncodeToPNG(), fileName, "image/png");
+
+        WWWForm form = new WWWForm();
+        //form.AddField("", "UnityImage.png",);
+        form.AddBinaryData("File", image.EncodeToJPG(), "UnityImage.jpg", "image/jpeg");
         
-        var www = UnityWebRequest.Post("https://parseapi.back4app.com/files/" + fileName + ".jpg", data);
+        //System.IO.File.WriteAllBytes(Application.persistentDataPath + "/testImage.png", image.EncodeToPNG());
+        //print(Application.persistentDataPath);
+
+
+        var www = UnityWebRequest.Post("https://parseapi.back4app.com/files/UnityImage.jpg", form);
         www.SetRequestHeader("X-Parse-Application-Id", parseApplicationId);
         www.SetRequestHeader("X-Parse-REST-API-Key", restApiKey);
-        www.SetRequestHeader("Content-Type", "image/jpeg");
+        //www.SetRequestHeader("Content-Type", "image/jpg");
         
         www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
-        if (www.isHttpError)
+        if (!www.isHttpError)
         {
-            print(www.error);
-        }
-        else
-        {
-            print(www.downloadHandler.text);
+            UploadImageData imageData = JsonConvert.DeserializeObject<UploadImageData>(www.downloadHandler.text);
+            StartCoroutine(DownloadImageFromServerCor(imageData.url));
+            print(imageData.name);
+            print(imageData.url);
+            StartCoroutine(AddVineCor(imageData.url, imageData.name, color, grape, country, region,
+                onEndAddDataCallback));
         }
     }
 
@@ -183,18 +260,17 @@ public class Back4appHelper : MonoBehaviour
             if (isFirst)
             {
                 isFirst = false;
-                result += string.Format("\"{0}\":\"{1}\"", key, query[key]);
+                if (query[key].StartsWith("{"))
+                    result += string.Format("\"{0}\":{1}", key, query[key]);
+                else 
+                    result += string.Format("\"{0}\":\"{1}\"", key, query[key]);
             }
             else
             {
                 if (query[key].StartsWith("{"))
-                {
                     result += string.Format(",\"{0}\":{1}", key, query[key]);
-                }
                 else
-                {
                     result += string.Format(",\"{0}\":\"{1}\"", key, query[key]);
-                }
             }
         }
         result += "}";
@@ -215,3 +291,78 @@ public class Back4appHelper : MonoBehaviour
         return www;
     }
 }
+
+public class AddDataCallback
+{
+    public long ResponseCode
+    {
+        get;
+        private set;
+    }
+    public string Json
+    {
+        get;
+        private set;
+    }
+    
+    public string ObjectId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public AddDataCallback(string json, long responseCode)
+    {
+        Json = json;
+        ResponseCode = responseCode;
+        ParseJson(json);
+    }
+
+    private void ParseJson(string json)
+    {
+        JObject jObject = JObject.Parse(json);
+        ObjectId = jObject["objectId"].ToString();
+        CreatedAt = DateTime.Parse(jObject["createdAt"].ToString());
+    }
+}
+
+public class GetDataCallback
+{
+    public long ResponseCode { get; private set; }
+    public string Json { get; private set; }
+    public List<JToken> Results { get; private set; }
+
+    public GetDataCallback(string json, long responseCode)
+    {
+        ResponseCode = responseCode;
+        Json = json;
+        Results = JObject.Parse(json)["results"].Children().ToList();
+    }
+
+    public void PrintResults()
+    {
+        foreach (var result in  Results)
+        {
+            Debug.Log(result.ToString());
+        }
+    }
+}
+
+public class UploadImageData
+{
+    [JsonProperty]
+    public string url;
+    [JsonProperty]
+    public string name;
+
+    [Newtonsoft.Json.JsonConstructor]
+    public UploadImageData(string url, string name)
+    {
+        this.url = url;
+        this.name = name;
+    }
+}
+
+/*public class DataPiece
+{
+    public string ObjectId { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdateAt { get; private set; }
+}*/
---
+++ Assets/_Scripts/MainManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    [SerializeField] private Back4appHelper _back4AppHelper;
+
+    public void AddColor(string color)
+    {
+        _back4AppHelper.AddColor(color, DebugAddDataCallback);
+    }
+
+    public void GetColors()
+    {
+        _back4AppHelper.GetColors(DebugGetDataCallback);
+    }
+
+    public void UploadImage(Texture2D image)
+    {
+        _back4AppHelper.AddVine(image, DebugAddDataCallback);
+    }
+
+    void DebugGetDataCallback(GetDataCallback data)
+    {
+     print(data.ResponseCode);
+     data.PrintResults();
+    }
+
+    private void DebugAddDataCallback(AddDataCallback data)
+    {
+        print(data.Json);
+        print(data.ResponseCode);
+        print(data.ObjectId);
+        print(data.CreatedAt);
+    }
+}
