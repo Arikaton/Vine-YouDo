35e8dd64b330ffe05181064919817716fd22adfb
Stanislav <99rsw99@mail.ru>
2020-10-18 15:27:59 +0300

    Make Camera Great Again

---
+++ Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private UIManager _uiManager;
+    [SerializeField] private RawImage image;
+    [SerializeField] GameObject cameraCanvas;
+    
+    private Texture2D _newPhoto;
+
+    public void TakePhoto()
+    {
+        NativeCamera.TakePicture(OnTakePicture, 512);
+    }
+
+    private void OnTakePicture(string path)
+    {
+        if (!string.IsNullOrEmpty(path))
+            _uiManager.ShowWindow(cameraCanvas);
+        _newPhoto = NativeCamera.LoadImageAtPath(path);
+        image.texture = _newPhoto;
+    }
+}
